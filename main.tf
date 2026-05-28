f69c1b47dc20dc7735674d8590921a6ed5a253b5
May Yadanar Maung / Cloud and DevOps skill set <nadihlaing776655@gmail.com>
2026-05-28 13:15:05 +0630

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,12 +16,56 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   restrict_public_buckets = true
 }
 
-resource "aws_cloudfront_origin_access_control" "orc" {
-  name                              = "orc"
-  description                       = "orc Policy"
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "oac"
+  description                       = "oac Policy"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.s3.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.orc.id
+    origin_id                = local.origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
 
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
@@ -76,47 +120,5 @@ resource "aws_s3_bucket_object" "js" {
   content_type = "application/javascript"
 }
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name              = aws_s3_bucket.s3.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.orc.id
-    origin_id                = local.origin_id
-  }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "Some comment"
-  default_root_object = "index.html"
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  price_class = "PriceClass_100"
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}
 
